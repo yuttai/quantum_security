e1e6947da9910803decd1e8c2623a28968efb485
yutta <yuttai@outlook.com>
2022-09-19 11:44:18 +0800

    The if statement

--- lecture.py
+++ lecture.py
@@ -2,12 +2,23 @@ def char2utf8(input_character):
     input_unicode = ord(input_character)
     input_binary = bin(input_unicode)
     input_binary_without_0b = input_binary[2:]
-    z = input_binary_without_0b[-6:]
-    y = input_binary_without_0b[-12:-6]
-    x = input_binary_without_0b[-16:-12]
-    input_utf8 = '0b1110{:0>4}10{}10{}'.format(x, y, z)
-    return hex(int(input_utf8, 2))
+    if len(input_binary_without_0b) <= 7:
+        return hex(input_unicode)
+    else:
+        z = input_binary_without_0b[-6:]
+        if len(input_binary_without_0b) <= 11:
+            pass
+        else:
+            y = input_binary_without_0b[-12:-6]
+            if len(input_binary_without_0b) <= 16:
+                x = input_binary_without_0b[-16:-12]
+                input_utf8 = '0b1110{:0>4}10{}10{}'.format(x, y, z)
+                return hex(int(input_utf8, 2))
+            else:
+                pass
 
 
 print(char2utf8('台'))
 print(char2utf8('台') == '0xe58fb0')
+print(char2utf8(' '))
+print(char2utf8(' ') == '0x20')
