cd2e5fffc6b905d2c97a93c01048cdf5f5e68338
戴淯琮 (Yu-Tsung Tai) <yuttai@indiana.edu>
2022-10-03 02:00:18 +0800

    Update lecture.py
    Only remainders are important to compute the gcd, so we change quotient_remainder_list to remainders...

--- lecture.py
+++ lecture.py
@@ -58,13 +58,12 @@ print(exp_mod_while(82, 29, 91))
 def gcd_while(a, b):
     if a < b:
         return gcd_while(b, a)
-    quotient_remainder_list = [divmod(a, b)]
-    while quotient_remainder_list[-1][1] != 0:
-        quotient_remainder_list.append(divmod(
-            quotient_remainder_list[-2][1] if len(quotient_remainder_list) > 1 else b,
-            quotient_remainder_list[-1][1]))
+    remainders = [a % b]
+    while remainders[-1] != 0:
+        remainders.append(
+            (remainders[-2] if len(remainders) > 1 else b) % remainders[-1])
     return \
-        quotient_remainder_list[-2][1] if len(quotient_remainder_list) > 1 else \
+        remainders[-2] if len(remainders) > 1 else \
         b
 
 
